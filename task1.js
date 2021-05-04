82de3d71ba3daa305520e1dfc634e9508ac84b4a
hafidz1605 <83576586+hafidz1605@users.noreply.github.com>
2021-05-04 09:50:25 +0700

    Add files via upload

--- task1.js
+++ task1.js
@@ -1,25 +1,33 @@
 let biodata = {
-    "name": "hafidz",
-    "age": 25,
-    "hobbies": ["reading","writting"],
-    "isMarried": false,
-    "schoolList": [
-                    {"name": "sdn4 buah batu",
-                    "yearIn": 2001,
-                    "yearout": 2007,
-                    "major": null},
+    name: "hafidz",
+    age: 25,
+    hobbies: ["reading","writting"],
+    isMarried: false,
+    schoolList: [
+                    {name: "sdn4 buah batu",
+                    yearIn: 2001,
+                    yearout: 2007,
+                    major: null},
 
-                    {"name": "smpn 18 bandung",
-                    "yearIn" : 2007,
-                    "yearout" : 20010,
-                    "major" : null},
+                    {name: "smpn 18 bandung",
+                    yearIn : 2007,
+                    yearout : 20010,
+                    major : null},
 
-                    {"name": "smk 7 Bandung",
-                    "yearIn" : 2010,
-                    "yearout" : 2013,
-                    "major" : "Farmasi"}
+                    {name: "smk 7 Bandung",
+                    yearIn : 2010,
+                    yearout : 2013,
+                    major : "Farmasi"}
                     
-    ]
+    ],
+    skills:  [   {skillname: "css",
+                level: "beginner"},
+                
+                {skillname: "javascrpts",
+                level: "beginner"} 
+            ],
+    interestInCoding: true
 }
+
 
 console.log(biodata)
